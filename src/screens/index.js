40f14cb6dec129dadf18cff55d391f89ffad9c22
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-17 20:06:54 +0300

    Refactored

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { View, Text } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import PolicyGeneratorOne from "./policyGenerator1";
+import PolicyGenerator from "./PolicyGenerator";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +10,7 @@ const Screens = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator  initialRouteName="Generator" >
-                <Stack.Screen name="Generator" component={PolicyGeneratorOne} />
+                <Stack.Screen name="Generator" component={PolicyGenerator} />
             </Stack.Navigator>
         </NavigationContainer>
     );
